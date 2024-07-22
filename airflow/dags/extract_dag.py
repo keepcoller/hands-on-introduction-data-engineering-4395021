7d253ad2d931238d350f8d7f196e772b285c49bc
Estevao Guevara <56946984+keepcoller@users.noreply.github.com>
2024-07-22 01:45:08 +0000

    added extract_dag

--- airflow/dags/extract_dag.py
+++ airflow/dags/extract_dag.py
@@ -4,21 +4,17 @@ from datetime import datetime
 from airflow.operators.bash import BashOperator
 from airflow import DAG
 
+# Default settings applied to all DAGs
 
-#Default settings applied to all tags
-
-with DAG (
-        dag_id = 'extract_dag',
-        description= 'Dag to show how to extract data',
-        schedule_interval=None,
-        catchup=False ) as dag:
+with DAG(
+        dag_id='extract_dag',
+        description='Dag to show how to extract data',
+        start_date=datetime(2024, 1, 1),
+        catchup=False) as dag:
     
-    extract_task = BashOperator (
-
+    extract_task = BashOperator(
         task_id='extract_task',
-        catchup=False 
-        bash_command= 'wget -c https://raw.githubusercontent.com/LinkedInLearning/hands-on-introduction-data-engineering-4395021/main/data/top-level-domain-names.csv -O /workspaces/hands-on-introduction-data-engineering-4395021/lab/manual/manual-extract.csv'
-
+        bash_command='wget -c https://raw.githubusercontent.com/LinkedInLearning/hands-on-introduction-data-engineering-4395021/main/data/top-level-domain-names.csv -O /workspaces/hands-on-introduction-data-engineering-4395021/lab/orchestrated/manual-extract.csv'
     )
-         
+   
 
